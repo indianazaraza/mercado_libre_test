dfc363309bdd8aaa38a82342cc5fcaaeed075392
Macarena Acosta <macarenaa988@gmail.com>
2022-03-31 21:53:23 -0300

    adding comments to methods

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -4,23 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
+/**
+ * Class that initializes all page classes
+ * */
 public class BasePage {
-    /**
-     * Class that initializes all page classes
-     * */
     private WebDriver driver;
 
     public BasePage(WebDriver driver){
         this.driver = driver;
     }
 
-    public void type(By locator, String text){
-        /**types given a text into an element given a locator*/
-        driver.findElement(locator).sendKeys(Keys.chord(text, Keys.ENTER));
+    /**types given a text into an element given a locator*/
+    public void type(String locator, String text){
+        driver.findElement(By.xpath(locator)).sendKeys(Keys.chord(text, Keys.ENTER));
     }
 
-    public void click(By locator){
-        /**clicks on an element given a locator*/
-        driver.findElement(locator).click();
+    /**clicks on an element given a locator*/
+    public void click(String locator){
+        driver.findElement(By.xpath(locator)).click();
+    }
+
+    /**get title current page*/
+    public String getTitle(){
+        return driver.getTitle();
     }
 }
